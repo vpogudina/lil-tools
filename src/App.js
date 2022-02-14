d3683745f5eab4e25fcd1784a2cc5eb47d9444e2
Valentina Pogudina <pogudina@amazon.com>
2022-02-14 02:33:15 -0500

    simple click-the-button to get agent logs

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import GetAgentLogs from './components/GetAgentLogs';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/get-log-chunk" component={GetAgentLogs} />
+          <Route path="/" component={GetAgentLogs} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
